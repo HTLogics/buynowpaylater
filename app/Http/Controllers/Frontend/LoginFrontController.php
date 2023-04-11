7ef7d1a366b13ec999076197dc8ba2a9a8d0c910
daljithtlogics <daljit@htlogics.com>
2023-04-11 15:55:08 +0530

    Change11April2023

--- app/Http/Controllers/Frontend/LoginFrontController.php
+++ app/Http/Controllers/Frontend/LoginFrontController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Session;
 
 class LoginFrontController extends Controller
 {
 
     public function __construct()
     {
-
+        $this->middleware('guest:admin');
     }
 
     public function index()
     {	
-	    $data = array();				
+	    $data = array();		
         return view('frontend.login', $data);
     }
 
