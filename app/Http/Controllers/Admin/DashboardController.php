ebaf5e09589d9f61cea3b05ac857f42e6ed151c7
daljithtlogics <daljit@htlogics.com>
2023-04-21 17:13:06 +0530

    ChangesApril2023

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,9 +8,8 @@ use App\Http\Controllers\Controller;
 use InvalidArgumentException;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Product;
-use App\Models\User;
+use App\Models\Customer;
 use App\Models\Order;
-use App\Models\Blog;
 use DB;
 class DashboardController extends Controller
 {
@@ -21,9 +20,24 @@ class DashboardController extends Controller
     }
 
     public function index()
-    {	
-	    $data = array();				
+    {
+		
+	    $data = array();
+        $data['totalproducts'] = Product::count();
+		$data['products'] = Product::orderBy('id', 'desc')->take(10)->get();
+		
+        $data['totalcutomers'] = Customer::count();
+	    $data['totalcutomers_last30days'] = Customer::where('created_at', '>', now()->subDays(30)->endOfDay())->count();
+		$data['customers'] = Customer::orderBy('id', 'desc')->take(10)->get();
+		
+        $data['totalorders'] = Order::count();
+		$data['totalorders_last30days'] = Order::where('created_at', '>', now()->subDays(30)->endOfDay())->count();
+        $data['completedorders'] = Order::where('status', 'completed')->count();
+        $data['pendingorders'] = Order::where('status', 'pending')->count();
+		$data['orderbymonths'] = Order::select(DB::raw("count(*) as count_per_month, date_format(created_at, '%M-%Y') as monthname"))->groupBy('monthname')->get();   
+				
         return view('admin.dashboard', $data);
+		
     }
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -27,36 +27,24 @@
 
 				<!--begin:: Widgets/Quick Stats-->
 				<div class="row row-full-height">
-					<div class="col-sm-12 col-md-12 col-lg-4">					
+					<div class="col-sm-12 col-md-12 col-lg-6">					
 						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-success">
 							<div class="kt-portlet__body kt-portlet__body--fluid">
 								<div class="kt-widget26">
 									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">19</span> 
+										<span class="kt-widget26__number">{{$pendingorders}}</span> 
 										<span class="kt-widget26__desc">Orders Pending!</span>
 									</div>									
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-sm-12 col-md-12 col-lg-4">						
-						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-warning">
-							<div class="kt-portlet__body kt-portlet__body--fluid">
-								<div class="kt-widget26">
-									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">10</span>
-										<span class="kt-widget26__desc">Orders Processing!</span>
-									</div>
-								</div>
-							</div>
-						</div>						
-					</div>
-					<div class="col-sm-12 col-md-12 col-lg-4">						
+					<div class="col-sm-12 col-md-12 col-lg-6">						
 						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-brand">
 							<div class="kt-portlet__body kt-portlet__body--fluid">
 								<div class="kt-widget26">
 									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">10</span>
+										<span class="kt-widget26__number">{{$completedorders}}</span>
 										<span class="kt-widget26__desc">Orders Completed!</span>
 									</div>
 								</div>
@@ -73,41 +61,29 @@
 
 				<!--begin:: Widgets/Quick Stats-->
 				<div class="row row-full-height">
-					<div class="col-sm-12 col-md-12 col-lg-4">					
+					<div class="col-sm-12 col-md-12 col-lg-6">					
 						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-success">
 							<div class="kt-portlet__body kt-portlet__body--fluid">
 								<div class="kt-widget26">
 									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">10</span> 
+										<span class="kt-widget26__number">{{$totalproducts}}</span> 
 										<span class="kt-widget26__desc">Total Products!</span>
 									</div>									
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-sm-12 col-md-12 col-lg-4">						
+					<div class="col-sm-12 col-md-12 col-lg-6">						
 						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-warning">
 							<div class="kt-portlet__body kt-portlet__body--fluid">
 								<div class="kt-widget26">
 									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">10</span>
+										<span class="kt-widget26__number">{{$totalcutomers}}</span>
 										<span class="kt-widget26__desc">Total Customers!</span>
 									</div>
 								</div>
 							</div>
 						</div>						
-					</div>
-					<div class="col-sm-12 col-md-12 col-lg-4">						
-						<div class="kt-portlet kt-portlet--height-fluid kt-portlet--border-bottom-brand">
-							<div class="kt-portlet__body kt-portlet__body--fluid">
-								<div class="kt-widget26">
-									<div class="kt-widget26__content">
-										<span class="kt-widget26__number">10</span>
-										<span class="kt-widget26__desc">Total Posts!</span>
-									</div>
-								</div>
-							</div>
-						</div>
 					</div>
 				</div>
 
@@ -125,7 +101,7 @@
 								<div class="kt-widget26">
 									<div class="c-info-box-area">
 										<div class="c-info-box box1">
-											<p>20</p>
+											<p>{{$totalcutomers_last30days}}</p>
 										</div>
 										<div class="c-info-box-content">
 											<h6 class="title">New Customers</h6>
@@ -142,7 +118,7 @@
 								<div class="kt-widget26">
 									<div class="c-info-box-area">
 										<div class="c-info-box box2">
-											<p>20</p>
+											<p>{{$totalcutomers}}</p>
 										</div>
 										<div class="c-info-box-content">
 											<h6 class="title">Total Customers</h6>
@@ -159,7 +135,7 @@
 								<div class="kt-widget26">
 									<div class="c-info-box-area">
 										<div class="c-info-box box3">
-											<p>20</p>
+											<p>{{$totalorders_last30days}}</p>
 										</div>
 										<div class="c-info-box-content">
 											<h6 class="title">Total Orders</h6>
@@ -176,7 +152,7 @@
 								<div class="kt-widget26">
 									<div class="c-info-box-area">
 										<div class="c-info-box box4">
-											<p>20</p>
+											<p>{{$totalorders}}</p>
 										</div>
 										<div class="c-info-box-content">
 											<h6 class="title">Total Orders</h6>
@@ -190,7 +166,99 @@
 				</div>
 				<!--end:: Widgets/Quick Stats-->
 			</div>
-		</div>				
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<!--begin:: Widgets/Order Statistics-->
+				<div class="kt-portlet kt-portlet--height-fluid">
+					<div class="kt-portlet__head">
+						<div class="kt-portlet__head-label">
+							<h3 class="kt-portlet__head-title">
+								Recent Items
+							</h3>
+						</div>
+					</div>
+					
+					<div class="kt-portlet__body kt-portlet__body--fluid">
+						<div class="kt-widget12">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<td style="width:30%">Name</td>
+											<td style="width:24%">Sku</td>
+											<td style="width:15%">Price</td>
+											<td style="width:15%">Status</td>
+											<td style="width:15%" class="kt-align-right">Action</td>
+										</tr>
+									</thead>
+									<tbody>
+									    @foreach($products as $product)
+										<tr style="line-height: 32px;">
+											<td>
+												<a href="{{route('admin.edit_item', $product->id)}}" class="kt-widget11__title">{{$product->name}}</a>
+											</td>
+											<td>{{$product->sku}}</td>
+											<td>{{$product->price}}</td>
+											<td>
+											@if($product->status == 1)
+												<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">Enable</span>
+											@else
+												<span class="kt-badge  kt-badge--danger kt-badge--inline kt-badge--pill">Disable</span>
+											@endif
+											</td>
+											<td class="kt-align-right kt-font-brand kt-font-bold"><a class="btn btn-sm btn-success" href="{{route('admin.edit_item', $product->id)}}"><i class="la la-eye"></i></a></td>
+										</tr>	
+										@endforeach
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!--end:: Widgets/Order Statistics-->
+			</div>
+			<div class="col-md-6">
+				<!--begin:: Widgets/Order Statistics-->
+				<div class="kt-portlet kt-portlet--height-fluid">
+					<div class="kt-portlet__head">
+						<div class="kt-portlet__head-label">
+							<h3 class="kt-portlet__head-title">
+								Recent Customers
+							</h3>
+						</div>
+					</div>
+					
+					<div class="kt-portlet__body kt-portlet__body--fluid">
+						<div class="kt-widget12">
+							<table class="table">
+									<thead>
+										<tr>
+											<td style="width:20%">First Name</td>
+											<td style="width:24%">Email</td>
+											<td style="width:25%">Join Date</td>
+											<td style="width:15%" class="kt-align-right">Action</td>
+										</tr>
+									</thead>
+									<tbody>
+									    @foreach($customers as $customer)
+										<tr style="line-height: 32px;">
+											<td>{{$customer->firstname}} {{$customer->lastname}}</td>									
+											<td>{{$customer->email}}</td>
+											<td>{{$customer->created_at}}</td>											
+											<td class="kt-align-right kt-font-brand kt-font-bold"><a class="btn btn-sm btn-success" href="{{route('admin.edit_customer', $customer->id)}}"><i class="la la-eye"></i></a></td>
+										</tr>	
+										@endforeach
+									</tbody>
+								</table>
+						</div>
+					</div>
+				</div>
+
+				<!--end:: Widgets/Order Statistics-->
+			</div>
+		</div>
 		<!--End::Dashboard 6-->
 	</div>
 
